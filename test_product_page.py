3f2768d02fb138d7c3cd04ce40b0e500ee096b71
SSV <s.simochenko@softinway.com>
2021-03-16 14:49:26 +0200

    adding to cart from product page

---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+from pages.product_page import ProductPage
+
+
+def test_user_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    # page.should_be_add_correct_product()
+    # page.should_be_correct_sum()
